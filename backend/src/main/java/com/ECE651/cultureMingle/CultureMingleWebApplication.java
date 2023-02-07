bbb1b6fe37e0eeb456354f3de4f847e8baa3e9c3
Aosen <aosenxiong@126.com>
2023-02-07 01:40:14 -0500

    Fix the connection to mongodb atlas

--- backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
+++ backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
@@ -1,13 +1,35 @@
 package com.ECE651.cultureMingle;
 
+import com.ECE651.cultureMingle.models.User;
+import com.ECE651.cultureMingle.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class CultureMingleWebApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CultureMingleWebApplication implements CommandLineRunner {
 
+	private final UserRepository userRepository;
+	@Autowired
+	public CultureMingleWebApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CultureMingleWebApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		if(userRepository.findAll().isEmpty()){
+
+			userRepository.save(new User("Aosen", "Xiong"));
+			userRepository.save(new User("Aric", "Xiong"));
+		}
+		for (User user : userRepository.findAll()) {
+			System.out.println(user);
+		}
+	}
 }
