26805d9d39417a4f535309c059b5b9d163393b69
Anqi Wang <a373wang@uwaterloo.ca>
2023-02-28 21:29:26 -0500

    CM-9: Enrich the Content of Event Details Page

--- frontend/culture-mingle/src/components/EventDetail/EventDetails.js
+++ frontend/culture-mingle/src/components/EventDetail/EventDetails.js
@@ -1,8 +1,6 @@
 import styles from './style.module.css'
 import 'antd/dist/reset.css';
-import { Layout, Space } from 'antd';
-import { Card } from 'antd';
-import { Button } from 'antd';
+import { Layout, Space, Card, Button, Avatar, Col, Row } from 'antd';
 import { ShareAltOutlined } from '@ant-design/icons';
 import { UsergroupAddOutlined } from '@ant-design/icons';
 import { useState } from 'react';
@@ -28,7 +26,7 @@ const footerStyle = {
 };
 
 export default function EventDetails() {
-    const [size, setSize] = useState('large');
+    const { Meta } = Card;
 
     return (
         <Space direction="vertical" style={{width: '100%',}} size={[0, 48]}>
@@ -44,33 +42,56 @@ export default function EventDetails() {
                 </Header>
             <Layout>
 
-            <Content style={contentStyle}>
+            <Content style={contentStyle} width='70%'>
                 <div className={styles.subtitles}>
                     Details
                 </div>
-                <div>
-                    
+                <div className={styles.description}>
+                    <p>The day before the Chinese New Year usually accompanied with a dinner feast, consisting of special meats are served at the tables, as a main course for the dinner and as an offering for the New Year. This meal is comparable to Thanksgiving dinner in the U.S. and remotely similar to Christmas dinner in other countries with a high percentage of Christians.</p>
+                    <p>In northern China, it is customary to make jiaozi, or dumplings, after dinner to eat around midnight. Dumplings symbolize wealth because their shape resembles a Chinese sycee. In contrast, in the South, it is customary to make a glutinous new year cake (niangao) and send pieces of it as gifts to relatives and friends in the coming days. Niángāo [Pinyin] literally means "new year cake" with a homophonous meaning of "increasingly prosperous year in year out".</p>                  
+                    <p>After dinner, some families may visit local temples hours before midnight to pray for success by lighting the first incense of the year; however in modern practice, many households held parties to celebrate. Traditionally, firecrackers were lit to ward evil spirits when the household doors sealed, and are not to be reopened until dawn in a ritual called "opening the door of fortune". A tradition of staying up late on Chinese New Year's Eve is known as shousui, which is still practised as it is thought to add on to one's parents' longevity.</p>
                 </div>
 
                 <div className={styles.subtitles}>
                     Attendees
                 </div>
+                <div className={styles.anttendees}>
+                    <Row gutter={16}>
+                        <Col className="gutter-row" span={'30%'}>
+                            <Card hoverable style={{ width: 240 }}>
+                                <Meta avatar={<Avatar size={50} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="User One" description="Organizer" />
+                            </Card>
+                        </Col>
+                        <Col className="gutter-row" span={'30%'}>
+                            <Card hoverable style={{ width: 240 }}>
+                                <Meta avatar={<Avatar size={50} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="User Two" description="Member" />
+                            </Card>
+                        </Col>
+                        <Col className="gutter-row" span={'30%'}>
+                            <Card hoverable style={{ width: 240 }}>
+                                <Meta avatar={<Avatar size={50} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="User Three" description="Member" />
+                            </Card>
+                        </Col>
+                    </Row>             
+                </div>
             </Content>
 
-            <Sider style={siderStyle} width={350}>
-                <Card className={styles.card}>
-                    Content
+            <Sider style={siderStyle} width={'30%'}>
+                <Card title="Location" extra={<a href="https://www.google.com/maps">Open in Google Map</a>} className={styles.card}>
+                    Student Life Centre, 200 University Ave W, Waterloo, ON N2L 3G1
                 </Card>
             </Sider>
 
             </Layout>
                 <Footer style={footerStyle}>
-                    <Button className={styles.button} type="primary" shape="round" icon={<ShareAltOutlined />} size={size}>
-                        Share
-                    </Button>
-                    <Button type="primary" shape="round" icon={<UsergroupAddOutlined />} size={size}>
-                        Join
-                    </Button>
+                    <div className={styles.buttons}>
+                        <Button className={styles.sharebutton} type="primary" icon={<ShareAltOutlined />} size={'large'}>
+                            Share
+                        </Button>
+                        <Button className={styles.joinbutton} type="primary" icon={<UsergroupAddOutlined />} size={'large'}>
+                            Join
+                        </Button>
+                    </div>
                 </Footer>
             </Layout>
         
