687ace1c8e1008d802cdf9fffdaab10bc15da53b
songbulabuka <39759657+songbulabuka@users.noreply.github.com>
2023-03-01 21:11:23 -0500

    Add css style for Login component

--- frontend/culture-mingle/src/components/Login/index.js
+++ frontend/culture-mingle/src/components/Login/index.js
@@ -1,6 +1,7 @@
+import "./login.css"
 import axios from 'axios';
 import { Layout, Space } from 'antd';
-import  { HeartFilled, HeartOutlined } from '@ant-design/icons';
+import { EditFilled, HeartFilled, HeartOutlined } from '@ant-design/icons';
 import { Button, Form, Input, Select, DatePicker } from 'antd';
 import { useState } from 'react';
 import moment from 'moment';
@@ -13,24 +14,24 @@ const headerStyle = {
     height: 64,
     paddingInline: 50,
     lineHeight: '64px',
-    backgroundColor: '#7dbcea',
+    backgroundColor: 'black',
 };
 const contentStyle = {
     minHeight: 500,
     lineHeight: '120px',
-    color: '#fff',
     backgroundColor: '#FFFFFF',
+    testAlign: 'center'
 };
 const siderStyle = {
     textalign: 'center',
     lineHeight: '120px',
     color: '#fff',
-    backgroundColor: '#3ba0e9',
+    backgroundColor: 'pink',
 };
 const footerStyle = {
     textAlign: 'center',
     color: '#fff',
-    backgroundColor: '#7dbcea',
+    backgroundColor: 'black',
 };
 const layout = {
     labelCol: {
@@ -51,10 +52,10 @@ const tailLayout = {
 
 const Login = () => {
     const [form] = Form.useForm();
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassWord] = useState("");
-    const [day,setDay] = useState("");
+    const [day, setDay] = useState("");
     const [gender, setGender] = useState("");
 
     // const [birthday,setBirthday] = useState(""); 
@@ -64,11 +65,11 @@ const Login = () => {
     //     console.log("birthday"+birthday);
     //   };
     const wholeform = {
-        name:name,
-        email:email,
-        password,password,
-        day:day,
-        gender:gender
+        name: name,
+        email: email,
+        password, password,
+        day: day,
+        gender: gender
     }
     const onFinish = (values) => {
         console.log(wholeform);
@@ -85,78 +86,90 @@ const Login = () => {
     return (
         <Space direction="vertical" style={{ width: '100%', }} size={[0, 48]}>
             <Layout>
-                <Header style={headerStyle}>Header</Header>
-                <Content style={contentStyle} >
-                    <div style={{ textalign: 'center', margin: 'auto', width: '50%', padding: '50px' }}>
-                        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish} style={{ maxWidth: 600, }}>
-                            <Form.Item name="name" label="Name" rules={[{ required: true, },]} onChange={(e)=>setName(e.target.value)}>
-                                <Input />
-                            </Form.Item>
-                            <Form.Item name="email" label="E-mail"
-                                rules={[
-                                    { required: true, message:"Please input your email address.",},
-                                    { type:'email', message:"Please input a valid email address."}
-                                ]}
-                                onChange={(e)=>setEmail(e.target.value)}
-                            >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item name='password' label="Password"
-                                rules={[
-                                    {required: true}
-                                ]}
-                                onChange={(e)=>setPassWord(e.target.value)}
-                            >
-                                <Input.Password/>
-                            </Form.Item>
-                            <Form.Item name='birthday' label="Date of birth" 
-                                rules={[
-                                    {required: true}
-                                ]}
-                            >
-                                <DatePicker onChange={(e)=>setDay(e.format("YYYY-MM-DD"))}></DatePicker>
-                            </Form.Item>
-                            <Form.Item name="gender" label="Gender" rules={[{ required: true, },]}>
-                                <Select placeholder="Select a option and change input text above" onChange={onGenderChange} allowClear>
-                                    <Option value="male">male</Option>
-                                    <Option value="female">female</Option>
-                                    <Option value="other">other</Option>
-                                </Select>
-                            </Form.Item>
+                <Header class="headerstyle">
+                    <Content></Content>
+                </Header>
+                <Layout>
+                    <Sider style={siderStyle} width={'10%'}></Sider>
+                    <Content style={contentStyle} >
+                        <div class="logo">
+                            <HeartFilled style={{ fontSize: '30px', color: 'pink' }} />
+                            CultureMingle
+                            <HeartFilled style={{ fontSize: '30px', color: 'pink' }} />
+                        </div>
+                        <div class="content">
+                            <Form {...layout} form={form} name="control-hooks" onFinish={onFinish} style={{ maxWidth: 600, fontFamily: 'fantasy', fontSize: '25px' }} size="large">
+                                <Form.Item name="name" label="Name" rules={[{ required: true, },]} onChange={(e) => setName(e.target.value)}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item name="email" label={<label>E-mail</label>}
+                                    rules={[
+                                        { required: true, message: "Please input your email address.", },
+                                        { type: 'email', message: "Please input a valid email address." }
+                                    ]}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item name='password' label="Password"
+                                    rules={[
+                                        { required: true }
+                                    ]}
+                                    onChange={(e) => setPassWord(e.target.value)}
+                                >
+                                    <Input.Password />
+                                </Form.Item>
+                                <Form.Item name='birthday' label="Date of birth"
+                                    rules={[
+                                        { required: true }
+                                    ]}
+                                >
+                                    <DatePicker onChange={(e) => setDay(e.format("YYYY-MM-DD"))}></DatePicker>
+                                </Form.Item>
+                                <Form.Item name="gender" label="Gender" rules={[{ required: true, },]}>
+                                    <Select placeholder="Select a option and change input text above" onChange={onGenderChange} allowClear>
+                                        <Option value="male">male</Option>
+                                        <Option value="female">female</Option>
+                                        <Option value="other">other</Option>
+                                    </Select>
+                                </Form.Item>
 
-                            <Form.Item
-                                noStyle
-                                shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
-                            >
-                                {({ getFieldValue }) =>
-                                    getFieldValue('gender') === 'other' ? (
-                                        <Form.Item
-                                            name="customizeGender"
-                                            label="Customize Gender"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                },
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                    ) : null
-                                }
-                            </Form.Item>
+                                <Form.Item
+                                    noStyle
+                                    shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+                                >
+                                    {({ getFieldValue }) =>
+                                        getFieldValue('gender') === 'other' ? (
+                                            <Form.Item
+                                                name="customizeGender"
+                                                label="Customize Gender"
+                                                rules={[
+                                                    {
+                                                        required: true,
+                                                    },
+                                                ]}
+                                            >
+                                                <Input />
+                                            </Form.Item>
+                                        ) : null
+                                    }
+                                </Form.Item>
 
-                            <Form.Item {...tailLayout}>
-                                <Button type="primary" htmlType="submit">
-                                    Submit
-                                </Button>
-                                <Button htmlType="button" onClick={onReset}>
-                                    Reset
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
-                </Content>
-                <Footer style={footerStyle}>Footer</Footer>
+                                <Form.Item {...tailLayout}>
+                                    <Button type="primary" htmlType="submit" style={{background:"pink"}}>
+                                        Submit
+                                    </Button>
+                                    <Button htmlType="button" onClick={onReset} style={{background:"white"}}>
+                                        Reset
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                            
+                        </div>
+                    </Content>
+                    <Sider style={siderStyle} width={'10%'}></Sider>
+                </Layout>
+                <Footer style={footerStyle}></Footer>
             </Layout>
         </Space>
     )
