ee150a51cb22345866fbfe458ee2f3e8293a6a75
Aosen <aosenxiong@126.com>
2023-03-05 15:42:59 -0500

    backend user management with okta

--- backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
+++ backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
@@ -1,55 +1,12 @@
 package com.ECE651.cultureMingle;
 
-import com.ECE651.cultureMingle.model.Activity;
-import com.ECE651.cultureMingle.model.Gender;
-import com.ECE651.cultureMingle.model.Profile;
-import com.ECE651.cultureMingle.repository.ActivityRepository;
-import com.ECE651.cultureMingle.repository.ProfileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class CultureMingleWebApplication implements CommandLineRunner {
-
-	private final ProfileRepository profileRepository;
-	private final ActivityRepository activityRepository;
-
-	@Autowired
-	public CultureMingleWebApplication(ProfileRepository profileRepository, ActivityRepository activityRepository) {
-		this.profileRepository = profileRepository;
-		this.activityRepository = activityRepository;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(CultureMingleWebApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		if(profileRepository.findAll().isEmpty()){
-			profileRepository.save(new Profile("Aosen", "Xiong", new SimpleDateFormat("yyyy-MM-dd").parse("1999-07-06"), Gender.NONBINARY, "I am Aosen Xiong!"));
-			profileRepository.save(new Profile("Ryan", "Cham", new SimpleDateFormat("yyyy-MM-dd").parse("2000-07-15"), Gender.MALE, "I am Ryan Cham!"));
-		}
-		for (Profile profile : profileRepository.findAll()) {
-			System.out.println(profile);
-		}
-
-		if(activityRepository.findAll().isEmpty()){
-			ArrayList<String> attendees = new ArrayList<>();
-			for (Profile profile : profileRepository.findAll()) {
-				attendees.add(profile.getId());
-			}
-			activityRepository.save(new Activity("Culture Mingle Meeting", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2023-03-03 20:00:00"), "Engineering 7", "Let's do it!", attendees));
-		}
-		for (Activity activity : activityRepository.findAll()) {
-			System.out.println(activity);
-		}
-	}
+@SpringBootApplication
+public class CultureMingleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CultureMingleWebApplication.class, args);
+    }
 }
+
