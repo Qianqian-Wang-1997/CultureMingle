9f6006b9f8799971c7568d82f83f4eab40cb19bc
Ryan CHAM <y5qin@uwaterloo.ca>
2023-03-04 20:55:37 -0500

    fix filename 2

---
+++ backend/src/main/java/com/ECE651/cultureMingle/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.ECE651.cultureMingle.repository;
+
+import com.ECE651.cultureMingle.model.Activity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends MongoRepository<Activity, String> {
+}
