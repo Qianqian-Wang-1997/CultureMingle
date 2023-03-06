01b1ac7f74f86cb061378d735871e9e7cb052312
Qianqian Wang <q3wang@uwaterloo.ca>
2023-03-05 21:12:20 -0500

    CM-8: Add basic file

---
+++ frontend/culture-mingle/src/components/EventList/EventComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function EventComponent() {
+    return (
+        <div>
+            eventcomponent
+        </div>
+    )
+}
+export default EventComponent 
--- frontend/culture-mingle/src/components/EventList/index.js
+++ frontend/culture-mingle/src/components/EventList/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import EventComponent from './EventComponent';
 function EventList() {
     return (
         <div>
-            eventlist
+            <EventComponent></EventComponent>
+            <EventComponent></EventComponent>
         </div>
     )
 }
