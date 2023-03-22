54db017eb21ed6cc65583ecec0d27e6b44e720fc
Ryan CHAM <y5qin@uwaterloo.ca>
2023-03-21 20:12:34 -0400

    CM-37: group

--- backend/src/main/java/com/ECE651/cultureMingle/controller/GroupController.java
+++ backend/src/main/java/com/ECE651/cultureMingle/controller/GroupController.java
@@ -1,2 +1,51 @@
-package com.ECE651.cultureMingle.controller;public class GroupController {
+package com.ECE651.cultureMingle.controller;
+
+import com.ECE651.cultureMingle.model.Group;
+import com.ECE651.cultureMingle.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GroupController {
+
+    @Autowired
+    private GroupService groupService;
+
+    @GetMapping("/groups")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<List<Group>> getAllGroup() {
+        return ResponseEntity.ok().body(groupService.getAllGroup());
+    }
+
+    @GetMapping("/groups/{id}")
+    @PreAuthorize("hasRole('USER')")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Group> getGroupById(@PathVariable String id) {
+        return ResponseEntity.ok().body(groupService.getGroupById(id));
+    }
+
+    @PostMapping("/groups")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Group> createGroup(@RequestBody Group group) {
+        return ResponseEntity.ok().body(groupService.createGroup(group));
+    }
+
+    @PutMapping("/groups/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Group> updateGroup(@PathVariable String id, @RequestBody Group group) {
+        group.setId(id);
+        return ResponseEntity.ok().body(groupService.updateGroup(group));
+    }
+
+    @DeleteMapping("/groups/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public HttpStatus deleteGroup(@PathVariable String id) {
+        groupService.deleteGroup(id);
+        return HttpStatus.OK;
+    }
 }
--- backend/src/main/java/com/ECE651/cultureMingle/service/GroupServiceImpl.java
+++ backend/src/main/java/com/ECE651/cultureMingle/service/GroupServiceImpl.java
@@ -1,2 +1,75 @@
-package com.ECE651.cultureMingle.service;public class GroupServiceImpl {
+package com.ECE651.cultureMingle.service;
+
+import com.ECE651.cultureMingle.exception.ResourceNotFoundException;
+import com.ECE651.cultureMingle.model.Group;
+import com.ECE651.cultureMingle.repository.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class GroupServiceImpl implements GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Override
+    public Group createGroup(Group group) {
+        return groupRepository.save(group);
+    }
+
+    @Override
+    public Group updateGroup(Group group) {
+        Optional<Group> groupDb = groupRepository.findById(group.getId());
+
+        if (groupDb.isPresent()) {
+            Group groupUpdate = groupDb.get();
+
+            if (group.getGroupName() != null) {
+                groupUpdate.setGroupName(group.getGroupName());
+            }
+            if (group.getOrganizer() != null) {
+                groupUpdate.setOrganizer(group.getOrganizer());
+            }
+            if (group.getLogoUrl() != null) {
+                groupUpdate.setLogoUrl(group.getLogoUrl());
+            }
+
+            groupRepository.save(group);
+            return groupUpdate;
+        } else {
+            throw new ResourceNotFoundException("Record not found with id : " + group.getId());
+        }
+    }
+
+    @Override
+    public List<Group> getAllGroup() {
+        return groupRepository.findAll();
+    }
+
+    @Override
+    public Group getGroupById(String id) {
+        Optional<Group> groupDb = groupRepository.findById(id);
+
+        if (groupDb.isPresent()) {
+            return groupDb.get();
+        } else {
+            throw new ResourceNotFoundException("Record not found with id : " + id);
+        }
+    }
+
+    @Override
+    public void deleteGroup(String id) {
+        Optional<Group> groupDb = groupRepository.findById(id);
+
+        if (groupDb.isPresent()) {
+            groupRepository.delete(groupDb.get());
+        } else {
+            throw new ResourceNotFoundException("Record not found with id : " + id);
+        }
+    }
 }
