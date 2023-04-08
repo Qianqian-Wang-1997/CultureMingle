3c8014260479b3a6b8b1b9c5c6b5b7a4ceff4e2e
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-04-08 17:14:06 -0400

    add user api and change signin reponse

--- backend/src/main/java/com/ECE651/cultureMingle/controller/UserController.java
+++ backend/src/main/java/com/ECE651/cultureMingle/controller/UserController.java
@@ -1,39 +1,19 @@
-//package com.ECE651.cultureMingle.controller;
-//
-//import com.ECE651.cultureMingle.model.User;
-//import com.ECE651.cultureMingle.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//public class UserController {
-//    @Autowired
-//    private UserService userService;
-//
-//    @GetMapping("/users/{id}")
-//    @CrossOrigin(origins = "http://localhost:3000")
-//    public ResponseEntity<User> getActivityById(@PathVariable String id) {
-//        return ResponseEntity.ok().body(userService.getUserById(id));
-//    }
-//
-//    @PostMapping("/users")
-//    @CrossOrigin(origins = "http://localhost:3000")
-//    public ResponseEntity<User> createUser(@RequestBody User user) {
-//        return ResponseEntity.ok().body(userService.createUser(user));
-//    }
-//
-//    @PutMapping("/users/{id}")
-//    @CrossOrigin(origins = "http://localhost:3000")
-//    public ResponseEntity<User> updateActivity(@PathVariable String id, @RequestBody User user) {
-//        user.setId(id);
-//        return ResponseEntity.ok().body(userService.updateUser(user));
-//    }
-//
-//    @DeleteMapping("/users/{id}")
-//    @CrossOrigin(origins = "http://localhost:3000")
-//    public HttpStatus deleteUser(@PathVariable String id) {
-//        userService.deleteUser(id);
-//        return HttpStatus.OK;
-//    }
-//}
+package com.ECE651.cultureMingle.controller;
+
+import com.ECE651.cultureMingle.model.User;
+import com.ECE651.cultureMingle.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
+    }
+}
