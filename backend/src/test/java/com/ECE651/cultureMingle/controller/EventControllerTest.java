1b6f4eaebc1e0fa20da2c5b7c34db10eca26047e
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-04-09 10:04:22 -0400

    update CI config for frontend test

---
+++ backend/src/test/java/com/ECE651/cultureMingle/controller/EventControllerTest.java
@@ -0,0 +1,37 @@
+package com.ECE651.cultureMingle.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllEvent() {
+    }
+
+    @Test
+    void getEventById() {
+    }
+
+    @Test
+    void createEvent() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void joinEvent() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+}
