9ba02a8be281036ca7d226c9054c7a20ac3c47de
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-04-09 01:02:03 -0400

    update CI config

--- backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
+++ backend/src/main/java/com/ECE651/cultureMingle/CultureMingleWebApplication.java
@@ -7,5 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CultureMingleWebApplication {
 
     public static void main(String[] args) { SpringApplication.run(CultureMingleWebApplication.class, args); }
-
 }
