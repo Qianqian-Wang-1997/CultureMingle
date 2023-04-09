c98cd212c54344c9c0d8e62476eadee88e6b83c4
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-04-09 13:50:31 -0400

    add more test cases

---
+++ backend/src/test/java/com/ECE651/cultureMingle/service/EventServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.ECE651.cultureMingle.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createEvent() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void joinEvent() {
+    }
+
+    @Test
+    void hostEvent() {
+    }
+
+    @Test
+    void bindEvent() {
+    }
+
+    @Test
+    void getAllEvent() {
+    }
+
+    @Test
+    void getEventById() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+}
---
+++ backend/src/test/java/com/ECE651/cultureMingle/service/GroupServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.ECE651.cultureMingle.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createGroup() {
+    }
+
+    @Test
+    void updateGroup() {
+    }
+
+    @Test
+    void joinGroup() {
+    }
+
+    @Test
+    void organizeGroup() {
+    }
+
+    @Test
+    void getAllGroup() {
+    }
+
+    @Test
+    void getGroupById() {
+    }
+
+    @Test
+    void deleteGroup() {
+    }
+}
