47eec57bbcec7284beb6df3cea03e810f6ebcdef
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-04-09 17:40:04 -0400

    add more backend test

--- backend/src/test/java/com/ECE651/cultureMingle/controller/AuthControllerTest.java
+++ backend/src/test/java/com/ECE651/cultureMingle/controller/AuthControllerTest.java
@@ -1,11 +1,42 @@
 package com.ECE651.cultureMingle.controller;
 
+import com.ECE651.cultureMingle.repository.RoleRepository;
+import com.ECE651.cultureMingle.repository.UserRepository;
+import com.ECE651.cultureMingle.security.jwt.JwtUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private PasswordEncoder encoder;
+
+    @MockBean
+    private JwtUtils jwtUtils;
 
     @BeforeEach
     void setUp() {
