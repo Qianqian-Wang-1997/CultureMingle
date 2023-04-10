780b36288183875c644088c89831352ae483ef1f
Qianqian Wang <q3wang@uwaterloo.ca>
2023-04-10 09:06:48 -0400

    Fix events unit test bug

--- frontend/culture-mingle/src/components/__test__/events.test.js
+++ frontend/culture-mingle/src/components/__test__/events.test.js
@@ -3,7 +3,7 @@ import { normalizeDate } from '../../store/events'; // Import the function to be
 describe('normalizeDate', () => {
   it('should correctly normalize a date', () => {
     const date = '2023-04-08T14:30:00Z'; // Input date
-    const expectedOutput = 'Saturday, April 08 at 10:30'; // Expected output after normalization
+    const expectedOutput = 'Saturday, April 08 at 14:30'; // Expected output after normalization
 
     const result = normalizeDate(date); // Call the function with the input date
     expect(result).toEqual(expectedOutput); // Assert that the actual output matches the expected output
